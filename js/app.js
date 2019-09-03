09fde003bf4e2b9bde9f80c6d24d9952263c24f6
valeriia4 <valeriia.borycheva@nure.ua>
2019-09-03 15:49:46 +0300

    add cv

---
+++ js/app.js
@@ -0,0 +1,56 @@
+"use strict";
+
+;
+
+(function ($) {
+  "use strict";
+
+  $('.portfolio-surfer__slider').slick({
+    slidesToShow: 1,
+    dots: true,
+    arrows: false
+  });
+  $('.portfolio-surfer__slidernext').slick({
+    slidesToShow: 1,
+    dots: true,
+    arrows: false
+  });
+  $('.portfolio-surfer__slidermonti').slick({
+    slidesToShow: 1,
+    dots: true,
+    arrows: false
+  }); //scroll
+
+  $(document).ready(function () {
+    $("#main-nav").on("click", "a", function (event) {
+      //отменяем стандартную обработку нажатия по ссылке
+      event.preventDefault(); //забираем идентификатор бока с атрибута href
+
+      var id = $(this).attr('href'),
+          //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top; //анимируем переход на расстояние - top за 1500 мс
+
+      $('body,html').animate({
+        scrollTop: top
+      }, 1500);
+    });
+  });
+  $(document).ready(function ($) {
+    var nav = $('.nav-container');
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 350) {
+        nav.addClass("f-nav");
+        $(".ba-header__img").css("display", "none");
+      } else {
+        nav.removeClass("f-nav");
+        $(".ba-header__img").css("display", "block");
+      }
+    });
+  });
+  $('.slider-logo').slick({
+    autoplay: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: false
+  });
+})(jQuery);
